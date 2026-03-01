47593e05e006c08173841677d01d177b309d070e
Thomas S. Binns <t.s.binns@outlook.com>
2026-03-01 20:49:40 +0000

    Update imports

--- mne_connectivity/viz/__init__.py
+++ mne_connectivity/viz/__init__.py
@@ -2,4 +2,9 @@
 
 from ._3d import plot_sensors_connectivity
 from .circle import plot_connectivity_circle
-from .line import plot_spectral_connectivity, plot_temporal_connectivity
+from .line import (
+    plot_epoch_connectivity,
+    plot_spectral_connectivity,
+    plot_temporal_connectivity,
+)
+from .matrix import plot_connectivity
